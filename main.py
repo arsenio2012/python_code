d8985b0fe2420bb984a76a83c8f59badf3c9c333
arsenio2012 <arseniomora2001@gmail.com>
2023-10-21 11:38:56 -0400

    Actualizacion, correciones

--- main.py
+++ main.py
@@ -81,14 +81,73 @@ def obtener_productos():
     
     listbox.delete(0, END)
     for producto in result:
-        producto_texto = f"ID: {producto[0]}, Nombre: {producto[1]}, Precio: {producto[2]}, Cantidad: {producto[3]}"
+        producto_texto = f"ID: {producto[0]}, Nombre: {producto[1]},  Cantidad: {producto[2]}, Precio: {producto[3]}"
         listbox.insert(END, producto_texto)
     listbox.config(width=60)
 
-def actualizar_productos():
+def contar_cantidad():
     seleccion = listbox.curselection() 
-   
-    print(f"hola estoy actalizando {seleccion}")
+    global entry_actualizar
+
+    if seleccion:
+        producto_seleccionado = listbox.get(seleccion)
+        try:
+          depurar = producto_seleccionado.replace('ID: ', '')
+          limpiar = depurar.split(',')
+          dato = limpiar[0]
+        except:
+            limpiar = producto_seleccionado[0]
+            dato = limpiar
+
+        print(dato)
+
+    else:
+        messagebox.showwarning("Advertencia", "No se ha seleccionado ningún producto")
+
+    nueva_cantidad = int(entry_actualizar.get())  # Obtener la cantidad ingresada
+    # Actualizar la cantidad sumando la nueva cantidad a la cantidad actual
+    mycursor.execute(f"SELECT cantidad FROM productos WHERE id_producto = {dato}")
+    cantidad_actual = int(mycursor.fetchone()[0])  # Convertir a entero
+    nueva_cantidad_total = cantidad_actual + nueva_cantidad
+
+    mycursor.execute(f"UPDATE productos SET cantidad = {nueva_cantidad_total} WHERE id_producto = {dato}")
+    mydb.commit()
+
+    messagebox.showinfo("Éxito", "Cantidad actualizada correctamente")
+    obtener_productos()
+
+# Función para descontar la cantidad de un producto
+def descontar_cantidad():
+    global entry_actualizar
+    seleccion = listbox.curselection()
+    if seleccion:
+        producto_seleccionado = listbox.get(seleccion)
+        try:
+            depurar = producto_seleccionado.replace('ID: ', '')
+            limpiar = depurar.split(',')
+            dato = limpiar[0]
+        except:
+            limpiar = producto_seleccionado[0]
+            dato = limpiar
+
+        nueva_cantidad = int(entry_actualizar.get())  # Obtener la cantidad ingresada
+        # Actualizar la cantidad restando la nueva cantidad de la cantidad actual
+        mycursor.execute(f"SELECT cantidad FROM productos WHERE id_producto = {dato}")
+        cantidad_actual = int(mycursor.fetchone()[0])  # Convertir a entero
+
+        # Condicion para cuando el stock (cantidad actual) no alcance 
+        if nueva_cantidad > cantidad_actual:
+            messagebox.showerror("Error", "No puedes descontar más de lo que tienes")
+        else:
+            nueva_cantidad_total = cantidad_actual - nueva_cantidad
+            mycursor.execute(f"UPDATE productos SET cantidad = {nueva_cantidad_total} WHERE id_producto = {dato}")
+            mydb.commit()
+
+            messagebox.showinfo("Éxito", "Cantidad actualizada correctamente")
+            obtener_productos()
+    else:
+        messagebox.showwarning("Advertencia", "No se ha seleccionado ningún producto")
+
 
 # Creación de la ventana principal
 root = Tk()
@@ -128,10 +187,6 @@ button_buscar.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
 button_eliminar = Button(root, text="Eliminar", width=20, command=eliminar_producto)
 button_eliminar.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
 
-# Botón para actualizar producto
-button_actualizar = Button(root, text="Actualizar", width=20,command=actualizar_productos )
-button_actualizar.grid(row=8, column=0, columnspan=2, padx=10, pady=10)
-
 # Lista de productos
 listbox = Listbox(root, width=60)
 listbox.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
@@ -144,6 +199,18 @@ scrollbar.config(command=listbox.yview)
 
 # Obtener y mostrar todos los productos al iniciar la aplicación
 obtener_productos()
+
+# Botón para actualizar producto
+label_contardescontar = Label(root, text="Cantidad contar/descontar:")
+label_contardescontar.grid(row=8, column=0, padx=10, pady=10)
+entry_actualizar = Entry(root, width=40)
+entry_actualizar.grid(row=8, column=1, padx=10, pady=10)
+
+button_contar = Button(root, text="Contar", width=20,command=contar_cantidad)
+button_contar.grid(row=9, column=0, padx=(10, 5), pady=10)
+
+button_descontar = Button(root, text="Descontar", width=20,command=descontar_cantidad )
+button_descontar.grid(row=9, column=1, padx=(5, 10), pady=10)
 
 # color de fondos 
 root.configure(background="dark blue")
